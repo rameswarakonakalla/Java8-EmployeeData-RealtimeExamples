94bd081e0cfae3fec77b2e50b86eb4109973c364
Konakalla <895207@cognizant.com>
2022-08-01 11:44:28 +0530

    first commit

---
+++ src/EmployeeOperations.java
@@ -0,0 +1,154 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class EmployeeOperations {
+
+   public static List<Employee> employeeList = new ArrayList<Employee>();
+    public static void main(String[] args) {
+
+
+
+        employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+        employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+        employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
+        employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
+        employeeList.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
+        employeeList.add(new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0));
+        employeeList.add(new Employee(177, "Manu Sharma", 35, "Male", "Account And Finance", 2010, 27000.0));
+        employeeList.add(new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0));
+        employeeList.add(new Employee(199, "Amelia Zoe", 24, "Female", "Sales And Marketing", 2016, 11500.0));
+        employeeList.add(new Employee(200, "Jaden Dough", 38, "Male", "Security And Transport", 2015, 11000.5));
+        employeeList.add(new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0));
+        employeeList.add(new Employee(222, "Nitin Joshi", 25, "Male", "Product Development", 2016, 28200.0));
+        employeeList.add(new Employee(233, "Jyothi Reddy", 27, "Female", "Account And Finance", 2013, 21300.0));
+        employeeList.add(new Employee(244, "Nicolus Den", 24, "Male", "Sales And Marketing", 2017, 10700.5));
+        employeeList.add(new Employee(255, "Ali Baig", 23, "Male", "Infrastructure", 2018, 12700.0));
+        employeeList.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
+        employeeList.add(new Employee(277, "Anuj Chettiar", 31, "Male", "Product Development", 2012, 35700.0));
+
+        maleFemaleEmployess();
+        allDepartments();
+        avgAgeofMaleFemale();
+        highestPaidEmployee();
+        employeeJoinedAfter2015();
+        countNumberOfEmployessInEachDept();
+        avgSalOfEachDept();
+        youngestMaleEmployeeProductDevelopment();
+        seniorMostEmployee();
+        countMaleFemaleEmployeesInSalesMarketing();
+        avgSalaryofMaleFemale();
+        employeeListByEachDept();
+        findAvgSAlTotalSal();
+        oldestEmployee();
+    }
+
+    public static void maleFemaleEmployess(){
+        System.out.println("How many male and female employees are there in the organization?");
+        Map<String, Long> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+       System.out.println(collect);
+
+    }
+
+    public static void allDepartments(){
+        System.out.println(" Print the name of all departments in the organization?");
+       employeeList.stream()
+                .map(Employee::getDepartment)
+                .distinct()
+                .forEach(System.out::println);
+
+    }
+    public static void avgAgeofMaleFemale(){
+        System.out.println("What is the average age of male and female employees?");
+
+
+        Map<String, Double> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getGender,
+                        Collectors.averagingInt(Employee::getAge)));
+        System.out.println(collect);
+    }
+
+    public static void highestPaidEmployee(){
+        System.out.println("Get the details of highest paid employee in the organization?");
+        Optional<Employee> collect = employeeList.stream()
+                .collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)));
+        System.out.println(collect);
+    }
+    public static void employeeJoinedAfter2015(){
+        System.out.println("Get the names of all employees who have joined after 2015");
+        employeeList.stream()
+                .filter(p -> p.getYearOfJoining() > 2015)
+                .map(employee -> employee.getName())
+                .forEach(System.out::println);
+    }
+
+    public static void countNumberOfEmployessInEachDept(){
+        System.out.println("Count the number of employees in each department?");
+        Map<String, Long> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        System.out.println(collect);
+    }
+
+    public static void avgSalOfEachDept(){
+        System.out.println("What is the average salary of each department?");
+        Map<String, Double> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+        System.out.println(collect);
+    }
+
+    public static void youngestMaleEmployeeProductDevelopment(){
+        System.out.println("Get the details of youngest male employee in the product development department?");
+
+        Optional<Employee> min = employeeList.stream()
+                .filter(p -> p.getGender().equalsIgnoreCase("MALE")
+                        && p.department.equalsIgnoreCase("Product Development"))
+                .min(Comparator.comparingInt(value -> value.getAge()));
+        System.out.println(min);
+    }
+    public static void seniorMostEmployee(){
+        System.out.println("Who has the most working experience in the organization?");
+        Optional<Employee> first = employeeList.stream()
+                .sorted(Comparator.comparingInt(value -> value.getYearOfJoining())).findFirst();
+        System.out.println(first);
+    }
+
+    public static void countMaleFemaleEmployeesInSalesMarketing(){
+        System.out.println("How many male and female employees are there in the sales and marketing team?");
+        Map<String, Long> sales_and_marketing = employeeList.stream()
+                .filter(employee -> employee.getDepartment().equalsIgnoreCase("Sales And Marketing"))
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+        System.out.println(sales_and_marketing);
+    }
+
+    public static void avgSalaryofMaleFemale(){
+        System.out.println("What is the average age of male and female employees?");
+
+
+        Map<String, Double> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getGender,
+                        Collectors.averagingDouble(Employee::getSalary)));
+        System.out.println(collect);
+    }
+
+    public static void employeeListByEachDept(){
+        System.out.println("List down the names of all employees in each department?");
+        System.out.println(employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment)));
+
+    }
+
+    public static void findAvgSAlTotalSal(){
+        System.out.println("What is the average salary and total salary of the whole organization");
+        DoubleSummaryStatistics collect = employeeList.stream()
+                .collect(Collectors.summarizingDouble(Employee::getSalary));
+        System.out.println("Average Sal : " + collect.getAverage());
+        System.out.println("Total Sal : " + collect.getSum());
+    }
+
+    public static void oldestEmployee(){
+        Optional<Employee> max = employeeList.stream()
+                .max(Comparator.comparingInt(value -> value.getAge()));
+        System.out.println("Name : " +max.get().getName() + "  Age " +max.get().getAge() + "    Department" + max.get().getDepartment());
+    }
+}
+
